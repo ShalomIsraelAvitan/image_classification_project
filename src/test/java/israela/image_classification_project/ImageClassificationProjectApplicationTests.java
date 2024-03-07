f9f0bfd67669f1ea54f5e9bc1505259ce81fa01d
ShalomIsraelAvitan <crovit214282@gmail.com>
2024-03-07 11:11:54 +0200

    first commit

---
+++ src/test/java/israela/image_classification_project/ImageClassificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package israela.image_classification_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageClassificationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
